43658b3c6257b632fcf4b4fed1176c0b68e75d11
omar sherif <oo.gmail.com>
2024-07-07 09:33:04 +0300

    add the transtion for the links inside the sidebar

--- src/script.js
+++ src/script.js
@@ -3,13 +3,14 @@ document.getElementById('mobile-menu-button').addEventListener('click', function
     var openIcon = document.getElementById('open-icon');
     if (sidebar.classList.contains('-left-full')) {
         sidebar.classList.remove('-left-full');
-        sidebar.classList.add('left-0');
+        sidebar.classList.add('left-0', 'open');
     }
 });
 
 document.getElementById('closeSidebarButton').addEventListener('click', function () {
     var sidebar = document.getElementById('sidebar');
-    sidebar.classList.remove('left-0');
+    sidebar.classList.remove('left-0', 'open');
     sidebar.classList.add('-left-full');
+
 });
 
